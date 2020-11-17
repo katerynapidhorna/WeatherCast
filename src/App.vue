13e07b865efeefa1494b2d2b4a8508276d9651e9
katerynapidhorna <katerynapidhorna@gmail.com>
2020-11-17 15:32:04 +0100

    render data in select field

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <SearchWeather msg="Welcome to Your Vue.js App" />
+    <SearchWeather />
   </div>
 </template>
 
--- src/components/SearchWeather.vue
+++ src/components/SearchWeather.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="search-container">
     <form>
-      <select name="country-code" id="country-code"></select>
+      <img class="weather-img" src="../img/weather-img.png" alt="" />
+      <select
+        class="country-code"
+        id="country-code"
+        v-model="selected"
+        v-if="data"
+      >
+        <option v-for="el in data" :key="el.code" :value="el.code">
+          {{ el.emoji }}
+          {{ el.code }}
+        </option>
+      </select>
       <div class="img-container"></div>
       <input
         class="text-input"
@@ -13,10 +24,25 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "SearchWeather",
-  props: {
-    msg: String,
+  data() {
+    return {
+      data: null,
+      selected: "NL",
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        `https://unpkg.com/country-flag-emoji-json@1.0.2/json/flag-emojis-by-code.pretty.json`
+      )
+      .then((response) => {
+        this.data = response.data;
+        console.log(this.data, "<==data");
+      });
   },
 };
 </script>
@@ -41,7 +67,7 @@ a {
 .search-container {
   width: fit-content;
   margin: auto;
-  margin-top: 25%;
+  margin-top: 20%;
 }
 
 form {
@@ -53,10 +79,18 @@ form {
   background-color: #f8f8f8;
   padding: 1em;
   border-radius: 12px;
-  filter: drop-shadow(0px 2px 10px #08153e);
+  box-shadow: 1px 3px 24px -13px #08153e;
 }
 
-form .img-container {
+.weather-img {
+  width: 49px;
+  padding-right: 15px;
+  height: 34px;
+  padding-left: 10px;
+  padding-top: 2px;
+}
+
+.img-container {
   position: absolute;
   right: 36px;
   top: 28px;
@@ -68,22 +102,36 @@ form .img-container {
   background-size: 41px;
 }
 
+.country-code {
+  width: 95px;
+  margin-right: 5px;
+  border-radius: 6px;
+  border: 1px solid #b4b8c5;
+  font-size: 18px;
+  font-weight: 900;
+}
+
+option {
+  font-weight: 900;
+}
+
 .text-input {
   width: 425px;
   padding: 1em 20px;
   border-radius: 6px;
   border: 1px solid #b4b8c5;
+  outline: none;
 }
 
 .text-input:hover {
   border: 1px solid #b5c7ff;
 }
 
-.text-input:active {
+.text-input:focus {
   border: 2px solid #b5c7ff;
 }
 
-.text-input:active .img-container {
+.text-input:focus .img-container {
   background-position-x: 0px;
 }
 </style>
