9ea50b6459d38dcef6fcf96f9ae232bca303557d
katerynapidhorna <katerynapidhorna@gmail.com>
2020-11-17 11:53:16 +0100

    add component with input field

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SearchWeather msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SearchWeather from "./components/SearchWeather.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SearchWeather,
   },
 };
 </script>
@@ -24,5 +23,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.8),
+      rgba(255, 255, 255, 0.8)
+    ),
+    linear-gradient(
+      119.25deg,
+      #102f7e -11.47%,
+      #0c8dd6 3.95%,
+      #1aa0ec 19.37%,
+      #60c6ff 34.78%,
+      #9bdbff 50.19%,
+      #b4deda 65.61%,
+      #ffd66b 81.02%,
+      #ffc178 96.44%,
+      #fe9255 111.85%
+    );
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
---
+++ src/components/SearchWeather.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="search-container">
+    <form>
+      <select name="country-code" id="country-code"></select>
+      <div class="img-container"></div>
+      <input
+        class="text-input"
+        type="text"
+        placeholder="Please enter your location..."
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchWeather",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.search-container {
+  width: fit-content;
+  margin: auto;
+  margin-top: 25%;
+}
+
+form {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: fit-content;
+  color: #b2b6c3;
+  background-color: #f8f8f8;
+  padding: 1em;
+  border-radius: 12px;
+  filter: drop-shadow(0px 2px 10px #08153e);
+}
+
+form .img-container {
+  position: absolute;
+  right: 36px;
+  top: 28px;
+  height: 27px;
+  width: 21px;
+  background-image: url("../img/search-icon.png");
+  background-position-x: -20px;
+  background-repeat: no-repeat;
+  background-size: 41px;
+}
+
+.text-input {
+  width: 425px;
+  padding: 1em 20px;
+  border-radius: 6px;
+  border: 1px solid #b4b8c5;
+}
+
+.text-input:hover {
+  border: 1px solid #b5c7ff;
+}
+
+.text-input:active {
+  border: 2px solid #b5c7ff;
+}
+
+.text-input:active .img-container {
+  background-position-x: 0px;
+}
+</style>
